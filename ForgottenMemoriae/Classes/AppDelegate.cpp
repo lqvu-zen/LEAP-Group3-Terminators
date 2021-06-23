c589fcad6116e097dc89b37a2420f5f582246146
HungBee <trieutanhung93@gmail.com>
2021-06-23 10:31:14 +0700

    + create splashscreen

--- ForgottenMemoriae/Classes/AppDelegate.cpp
+++ ForgottenMemoriae/Classes/AppDelegate.cpp
@@ -23,7 +23,7 @@
  ****************************************************************************/
 
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
+#include "SplashScene.h"
 
 // #define USE_AUDIO_ENGINE 1
 
@@ -34,10 +34,10 @@ using namespace cocos2d::experimental;
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+static cocos2d::Size designResolutionSize = cocos2d::Size(960, 540);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(960, 540);
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(1920, 1080);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(3840, 2160);
 
 AppDelegate::AppDelegate()
 {
@@ -80,9 +80,10 @@ bool AppDelegate::applicationDidFinishLaunching() {
         director->setOpenGLView(glview);
     }
 
+    /*
     // turn on display FPS
     director->setDisplayStats(true);
-
+    */
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0f / 60);
 
@@ -108,7 +109,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+    auto scene = SplashScene::createScene();
 
     // run
     director->runWithScene(scene);
---
+++ ForgottenMemoriae/Classes/SplashScene.cpp
@@ -0,0 +1,84 @@
+/****************************************************************************
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+
+ http://www.cocos2d-x.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+#include "SplashScene.h"
+#include "ui/CocosGUI.h"
+#include "MainMenuScene.h"
+#include "Definitions.h"
+
+USING_NS_CC;
+
+Scene* SplashScene::createScene()
+{
+    return SplashScene::create();
+}
+
+
+// on "init" you need to initialize your instance
+bool SplashScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if (!Scene::init())
+    {
+        return false;
+    }
+
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+
+    // add a "close" icon to exit the progress. it's an autorelease object
+
+    auto bar = Sprite::create("sprites/bar.png");
+    bar->setPosition(Vec2(visibleSize.width * 0.5, visibleSize.height * 0.5));
+    this->addChild(bar);
+
+    auto loadingBar = ui::LoadingBar::create("sprites/loadingbar.png");
+    loadingBar->setPosition(Vec2(visibleSize.width * 0.5, visibleSize.height * 0.25));
+    loadingBar->setScale(1.5);
+    loadingBar->setDirection(ui::LoadingBar::Direction::LEFT);
+    loadingBar->setPercent(0);
+    this->addChild(loadingBar);
+    this->schedule([=](float delta) {
+        float percent = loadingBar->getPercent();
+        percent++;
+        loadingBar->setPercent(percent);
+        if (percent >= 100.0f) {
+            this->unschedule("updateLoadingBar");
+            this->scheduleOnce(CC_SCHEDULE_SELECTOR(SplashScene::goToMainMenuScene), DISPLAY_TIME_SPLASH_SCENE);
+        }
+    }, 0.1f, "updateLoadingBar");
+
+
+    return true;
+}
+
+void SplashScene::goToMainMenuScene(float dt) {
+    auto scene = MainMenuScene::createScene();
+    Director::getInstance()->replaceScene(TransitionFade::create(TRANSITION_TIME, scene));
+}
