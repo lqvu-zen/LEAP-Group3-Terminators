53d43a1f9d7cbc09961dc1a6e81cdc5527c3f72b
Trịnh Đông Dương <dongduong.trinh@gmail.com>
2021-06-28 18:13:06 +0700

    - test follow camera (Now working with Physic layer removed from the PlayGameScene)

--- ForgottenMemoriae/Classes/PlayGameScene.cpp
+++ ForgottenMemoriae/Classes/PlayGameScene.cpp
@@ -1,18 +1,18 @@
 /****************************************************************************
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,38 +28,35 @@ USING_NS_CC;
 
 Scene* PlayGameScene::createScene()
 {
-	auto scene = Scene::createWithPhysics();
+	auto scene = PlayGameScene::create();
 	scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
 	scene->getPhysicsWorld()->setGravity(Vect(0, 0));//test world with gravity physics!!! Working for now!!!
-	auto layer = PlayGameScene::create();
-	layer->SetPhysicsWorld(scene->getPhysicsWorld());
-	scene->addChild(layer);
-    return scene;
+	return scene;
 }
 
 // Print useful error message instead of segfaulting when files are not there.
 static void problemLoading(const char* filename)
 {
-    printf("Error while loading: %s\n", filename);
-    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+	printf("Error while loading: %s\n", filename);
+	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
 }
 
 // on "init" you need to initialize your instance
 bool PlayGameScene::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Scene::initWithPhysics() )
-    {
-        return false;
-    }
+	//////////////////////////////
+	// 1. super init first
+	if (!Scene::initWithPhysics())
+	{
+		return false;
+	}
 
-    visibleSize = Director::getInstance()->getVisibleSize();
-    origin = Director::getInstance()->getVisibleOrigin();
+	visibleSize = Director::getInstance()->getVisibleSize();
+	origin = Director::getInstance()->getVisibleOrigin();
 	//add background
 	auto bgSprite = Sprite::create("sprites/Top sky.png");
 	bgSprite->setPosition(Point(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
-	bgSprite->setScale(visibleSize.width /bgSprite->getContentSize().width, visibleSize.height / bgSprite->getContentSize().height);
+	bgSprite->setScale(visibleSize.width / bgSprite->getContentSize().width, visibleSize.height / bgSprite->getContentSize().height);
 	this->addChild(bgSprite);
 
 	//collision with map edges
@@ -68,11 +65,11 @@ bool PlayGameScene::init()
 	edgeNode->setPosition(Point(center->x,center->y));
 	edgeNode->setPhysicsBody(edgeBody);
 	this->addChild(edgeNode);*/
-	
+
 	//map setup + add map
 	map = TMXTiledMap::create("map/playMap.tmx");
-	addChild(map, 0);
-	
+	this->addChild(map, 0);
+
 	//setup map physics. Since we are doing a 60x34 map so width = 60 and height = 34 (2 loops)
 	TMXLayer *Foreground = map->getLayer("Foreground");
 	for (int i = 0; i < 100; i++)
@@ -99,26 +96,24 @@ bool PlayGameScene::init()
 	ValueMap spawnPoint = objectGroup->getObject("SpawnPoint");
 	int x = spawnPoint["x"].asInt();
 	int y = spawnPoint["y"].asInt();
-	
+
 	//Add character here!!!
 	player = Sprite::create("sprites/yellowbird-midflap.png");
-	player->setPosition(Point(x,y));
+	player->setPosition(Point(x, y));
 	auto playerBody = PhysicsBody::createBox(player->getContentSize());
 	player->setPhysicsBody(playerBody);
-	CCLOG("width: %f; height: %f",visibleSize.width, visibleSize.height);
-	cameraTarget = Sprite::create();
+	CCLOG("width: %f; height: %f", visibleSize.width, visibleSize.height);
+	cameraTarget = Node::create();
 	cameraTarget->setPositionX(player->getPositionX());
 	cameraTarget->setPositionY(visibleSize.height / 2 + origin.y);
-	cameraTarget->retain();
 
 	this->addChild(player);
-	this->schedule(CC_SCHEDULE_SELECTOR(PlayGameScene::updateScene));
 	this->addChild(cameraTarget);
 
-	
+
 	//this->setViewPointCenter(player->getPositionX(), player->getPositionY());
-	
-	
+
+
 	//Add enemies here!!
 	//Algorithm: get the EnemySpawn ValueMap from the objectGroup then check if the EnemySpawn has the value "Enemy == 1".
 	//If true -> add enemey at the EnemySpawn.
@@ -131,8 +126,8 @@ bool PlayGameScene::init()
 			this->addEnemyAt(eneX, eneY);
 		}
 	}
-	
-	
+
+
 	//Keyboard test
 	auto listener = EventListenerKeyboard::create();
 	listener->onKeyPressed = CC_CALLBACK_2(PlayGameScene::onKeyPressedTest, this);
@@ -141,11 +136,11 @@ bool PlayGameScene::init()
 	mapSize = map->getMapSize().width * map->getTileSize().width;
 	CCLOG("Map size: %f", mapSize);
 
-	followCamera = Follow::create(cameraTarget, Rect::ZERO);
-	followCamera->retain();
+	followCamera = Follow::create(cameraTarget);
 	this->runAction(followCamera);
-	
-    return true;
+	this->scheduleUpdate();
+
+	return true;
 }
 
 
@@ -198,9 +193,7 @@ void PlayGameScene::setViewPointCenter(float positionX, float positionY)
 	this->setPosition(viewPoint);
 }
 
-void PlayGameScene::updateScene(float dt)
+void PlayGameScene::update(float dt)
 {
 	cameraTarget->setPositionX(player->getPositionX());
-	//this->runAction(Follow::create(cameraTarget, Rect::ZERO));
-	//map->runAction(Follow::createWithOffset(player, -(map->getMapSize().width * map->getTileSize().width)/2, -(map->getMapSize().height * map->getTileSize().height)/2));
 }
--- ForgottenMemoriae/Classes/PlayGameScene.h
+++ ForgottenMemoriae/Classes/PlayGameScene.h
@@ -1,18 +1,18 @@
 /****************************************************************************
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,24 +30,22 @@ USING_NS_CC;
 class PlayGameScene : public cocos2d::Scene
 {
 public:
-	Sprite *cameraTarget;
+	Node *cameraTarget;
 	cocos2d::Follow *followCamera;
-    static cocos2d::Scene* createScene();
+	static cocos2d::Scene* createScene();
 	virtual bool init();
 	void setViewPointCenter(float positionX, float positionY);
-	void updateScene(float dt);
-    CREATE_FUNC(PlayGameScene);
+	virtual void update(float dt);
+	CREATE_FUNC(PlayGameScene);
 
 private:
 	TMXTiledMap *map;
 	cocos2d::Size visibleSize;
 	cocos2d::Vec2 origin;
-	void SetPhysicsWorld(cocos2d::PhysicsWorld *world) { sceneWorld = world; };
-	cocos2d::PhysicsWorld *sceneWorld;
 	Sprite *player;
 	void addEnemyAt(int x, int y);
 	void onKeyPressedTest(EventKeyboard::KeyCode keyCode, Event *event);
-	
+
 };
 
 #endif // __PLAYGAME_SCENE_H__
