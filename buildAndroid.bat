d0eeb49d5465ab553fabbe279a30d10e055b304f
HungBee <trieutanhung93@gmail.com>
2021-07-02 15:09:26 +0700

    - Clear branch

--- buildAndroid.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call config.bat
-
-cocos compile -s .\%PROJECT_NAME% -p android -m release -o .\%PROJECT_NAME%\bin --ap android-28 --app-abi armeabi-v7a:x86 --no-sign
-
-pause
--- runWin32.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call config.bat
-
-cocos run -s .\%PROJECT_NAME% -p win32 -m release
-
-pause
