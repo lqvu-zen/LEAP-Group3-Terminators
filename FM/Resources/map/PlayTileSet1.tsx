8628c12192533a7302c02cb71a990bdbd2af53c9
Trịnh Đông Dương <dongduong.trinh@gmail.com>
2021-07-08 17:41:34 +0700

    - !Clear branch laptop error

--- FM/Resources/map/PlayTileSet1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="PlayTileSet1" tilewidth="16" tileheight="16" tilecount="324" columns="18">
- <image source="[TILESET] Tileset.png" width="300" height="300"/>
-</tileset>
