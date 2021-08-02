b8a5eb3e11a45e8b4e4709f6c51c952ae90755af
Quang-Vu <lqvu.zen@gmail.com>
2021-08-02 11:52:22 +0700

    *push for merge

--- runAndroid.bat
+++ runAndroid.bat
@@ -2,6 +2,6 @@
 
 call config.bat
 
-cocos run -s .\%PROJECT_NAME% -p android -m release --ap android-28 --app-abi armeabi-v7a:x86 --no-sign
+cocos run -s .\%PROJECT_NAME% -p android -m release --ap android-28 --app-abi armeabi-v7a:x86 
 
 pause
